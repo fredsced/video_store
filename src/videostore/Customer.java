403574f29bda282e4f706909f957810110a4d919
fred <frederic.descloux@gmail.com>
2025-01-20 21:33:31 +0100

    replace temp with query: thisAmount by each.getCharge()

--- src/videostore/Customer.java
+++ src/videostore/Customer.java
@@ -25,16 +25,14 @@ public class Customer {
         String result = "Rental Record for " + getName() + "\n";
         for (Rental each : rentals) {
 
-            double thisAmount =  each.getCharge();
-
             frequentRenterPoints ++;
 
             if((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
                 frequentRenterPoints ++;
 
-            result += "\t" + each.getMovie().getTitle() + '\t' + String.valueOf(thisAmount) + "\n";
+            result += "\t" + each.getMovie().getTitle() + '\t' + String.valueOf(each.getCharge()) + "\n";
 
-            totalAmount += thisAmount;
+            totalAmount += each.getCharge();
 
         }
         result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
