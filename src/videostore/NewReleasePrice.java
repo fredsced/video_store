cde2edb5df0d5525e3b55a2451dc1619824d8169
fred <frederic.descloux@gmail.com>
2025-01-22 23:00:17 +0100

    replace conditional with polymorphism

--- src/videostore/NewReleasePrice.java
+++ src/videostore/NewReleasePrice.java
@@ -6,5 +6,9 @@ public class NewReleasePrice extends Price {
     int getPrice() {
         return Movie.NEW_RELEASE;
     }
+    @Override
+    double getCharge(int daysRented){
+        return daysRented * 3;
+    }
 
 }
--- src/videostore/RegularPrice.java
+++ src/videostore/RegularPrice.java
@@ -5,4 +5,11 @@ public class RegularPrice extends Price {
     int getPrice() {
         return Movie.REGULAR;
     }
+    @Override
+    double getCharge(int daysRented){
+        double result = 2;
+        if (daysRented > 2)
+            result += (daysRented - 2) * 1.5;
+        return result;
+    }
 }
