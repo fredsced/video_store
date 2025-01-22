4dc02885319bb8c5390b1643384d13b2f2286a43
fred <frederic.descloux@gmail.com>
2025-01-22 22:50:22 +0100

    state: move getCharge method to Price

--- src/videostore/Price.java
+++ src/videostore/Price.java
@@ -2,4 +2,24 @@ package videostore;
 
 public abstract class Price {
     abstract int getPrice();
+
+    double getCharge(int daysRented){
+        double result = 0;
+        switch (getPrice()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (daysRented > 2)
+                    result += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                result += daysRented * 3;
+                break;
+            case Movie.CHILDREN:
+                result += 1.5;
+                if (daysRented > 3)
+                    result += (daysRented - 3) * 1.5;
+                break;
+        }
+        return result;
+    }
 }
