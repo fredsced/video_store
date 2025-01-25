beb8bb60fa1e7b4de084cfb86cef47eb3418018b
fred <frederic.descloux@gmail.com>
2025-01-25 21:41:26 +0100

    test change of state

--- src/Main.java
+++ src/Main.java
@@ -10,12 +10,21 @@ public class Main {
         Rental rental = new Rental(movie, 3);
         Customer customer = new Customer("John");
 
+
         customer.addRental(rental);
 
         System.out.println(customer.statement());
         System.out.println("------------------------------");
         System.out.println("------------------------------");
         System.out.println(customer.htmlStatement());
+
+        System.out.println("------------------------------");
+        System.out.println("------------------------------");
+        System.out.println("---------WHEN STAR WAR BECOME REGULAR---------------------");
+        movie.setPriceCode(0);
+        System.out.println(customer.statement());
+
+
 
     }
 }
